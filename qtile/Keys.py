3e53fc4c358f5294407c5f2d4109b20d155ac57d
yulian david <julianolomanolox@hotmail.com>
2023-03-26 20:46:27 -0500

    trying to organize everything

---
+++ qtile/Keys.py
@@ -0,0 +1,63 @@
+from libqtile.config import Key
+from libqtile.lazy import lazy
+
+def keys():
+    mod = "mod4"
+    terminal = "alacritty"
+    keys = [
+    Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
+    Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
+    Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
+    Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
+    Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
+    Key(
+        [mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"
+    ),
+    Key(
+        [mod, "shift"],
+        "l",
+        lazy.layout.shuffle_right(),
+        desc="Move window to the right",
+    ),
+    Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
+    Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
+    Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
+    Key(
+        [mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"
+    ),
+    Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
+    Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
+    Key([mod], "n", lazy.layout.reset(), desc="Reset all window sizes"),
+    Key([mod, "control"], "f", lazy.window.toggle_floating()),
+    Key(
+        [mod, "shift"],
+        "Return",
+        lazy.layout.toggle_split(),
+        desc="Toggle between split and unsplit sides of stack",
+    ),
+    Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
+    # Toggle between different layouts as defined below
+    Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
+    Key([mod], "w", lazy.window.kill(), desc="Kill focused window"),
+    Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
+    Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
+    Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
+    # keybinds for the programs i usualy use
+    Key([mod, "control"], "f", lazy.spawn("firefox"), desc="navegador wtf"),
+    Key([mod, "control"], "d", lazy.spawn("discord"), desc="discord"),
+    Key([mod, "control"], "a", lazy.spawn("android-studio"), desc="android studio"),
+    Key([mod, "control"], "v", lazy.spawn("code"), desc="vscode"),
+    Key([mod, "control"], "s", lazy.spawn("spotify"), desc="spotify"),
+    Key([mod, "control"], "g", lazy.spawn("github-desktop"), desc="github"),
+    Key([mod, "control"], "k", lazy.spawn("kmix"), desc="volume control"),
+    Key([mod, "control"], "p", lazy.spawn("postman"), desc="postman"),
+    Key(
+        [mod, "shift"],
+        "i",
+        lazy.spawn("intellij-idea-ultimate-edition"),
+        desc="everton",
+    ),
+    # keybinds for my work flow
+]
+
+    return keys
--- qtile/Widgets.py
+++ qtile/Widgets.py
@@ -1,97 +1,9 @@
-import os
-import re
-from datetime import datetime
-from typing import Optional
+from Colors import ccolors
+from libqtile import  widget
 
-import fontawesome
-import psutil
-from libqtile import bar, layout, widget
-from libqtile.config import Click,Drag,Key, Match,Screen
-from libqtile.lazy import lazy
-from libqtile.widget.textbox import TextBox
-#import own modules
-from Colors import colors
-from Groups import groups
-from Keys import keys
-colors = colors()
-groups = groups()
-kkeys = keys() 
-mod = "mod4"
-
-today = datetime.today()
-
-keys = kkeys 
-
-for i in groups:
-    keys.extend(
-        [
-            # mod1 + letter of group = switch to group
-            Key(
-                [mod],
-                i.name,
-                lazy.group[i.name].toscreen(),
-                desc="Switch to group {}".format(i.name),
-            ),
-            # mod1 + shift + letter of group = switch to & move focused window to group
-            Key(
-                [mod, "shift"],
-                i.name,
-                lazy.window.togroup(i.name, switch_group=True),
-                desc="Switch to & move focused window to group {}".format(i.name),
-            ),
-            # Or, use below if you prefer not to switch to that group.
-            # # mod1 + shift + letter of group = move focused window to group
-            # Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
-            #     desc="move focused window to group {}".format(i.name)),
-        ]
-    )
-
-layouts = [
-    layout.Columns(
-        border_focus=colors["cascade3"],
-        border_width=2,
-        margin=11,
-    ),
-    layout.Max(
-        border_focus=colors["cascade3"],
-        border_width=2,
-        margin=6,
-    ),
-    # Try more layouts by unleashing below layouts.
-    # layout.Stack(num_stacks=2),
-    # layout.Bsp(),
-    # layout.Matrix(),
-    # layout.MonadTall(),
-    # layout.MonadWide(),
-    # layout.RatioTile(),
-    # layout.Tile(),
-    # layout.TreeTab(),
-    # layout.VerticalTile(),
-    # layout.Zoomy(),
-]
-
-widget_defaults = dict(
-    font="sans",
-    fontsize=12,
-    padding=10,
-)
-
-extension_defaults = widget_defaults.copy()
-
-
-def my_func(text):
-    text = text.rpartition("/")
-    if text:
-        text = text[-1]
-    elif not text:
-        text = text.rpartition("-")[-1]
-    return text
-
-
-screens = [
-    Screen(
-        top=bar.Bar(
-            [
+def widgets():
+    colors = ccolors()
+    widgets = [
                 widget.GroupBox(
                     borderwidth=8,
                     fontsize=15,
@@ -104,7 +16,7 @@ screens = [
                     this_current_screen_border=colors["cascade3"],
                 ),
                 widget.Spacer(length=120),
-                widget.WindowName(parse_text=my_func),
+                widget.WindowName(),
                 widget.TextBox(
                     "\uf0d9", fontsize=55, padding=-8, foreground=colors["cascade6"]
                 ),
@@ -219,71 +131,5 @@ screens = [
                     foreground=colors["white"],
                     fontsize=26,
                 ),
-            ],
-            26,
-            margin=10,
-            background=colors["background"],
-        ),
-    ),
-]
-
-# Drag floating layouts.
-mouse = [
-    Drag(
-        [mod],
-        "Button1",
-        lazy.window.set_position_floating(),
-        start=lazy.window.get_position(),
-    ),
-    Drag(
-        [mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()
-    ),
-    Click([mod], "Button2", lazy.window.bring_to_front()),
-]
-
-dgroups_key_binder = None
-dgroups_app_rules = []  # type: list
-follow_mouse_focus = True
-bring_front_click = False
-cursor_warp = False
-floating_layout = layout.Floating(
-    float_rules=[
-        # Run the utility of `xprop` to see the wm class and name of an X client.
-        *layout.Floating.default_float_rules,
-        Match(wm_class="confirmreset"),  # gitk
-        Match(wm_class="makebranch"),  # gitk
-        Match(wm_class="maketag"),  # gitk
-        Match(wm_class="ssh-askpass"),  # ssh-askpass
-        Match(title="branchdialog"),  # gitk
-        Match(title="pinentry"),  # GPG key password entry
-    ]
-)
-auto_fullscreen = True
-focus_on_window_activation = "smart"
-reconfigure_screens = True
-
-# If things like steam games want to auto-minimize themselves when losing
-# focus, should we respect this or not?
-auto_minimize = True
-
-# When using the Wayland backend, this can be used to configure input devices.
-wl_input_rules = None
-
-# XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
-# string besides java UI toolkits; you can see several discussions on the
-# mailing lists, GitHub issues, and other WM documentation that suggest setting
-# this string if your java app doesn't work correctly. We may as well just lie
-# and say that we're a working one by default.
-#
-# We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
-# java that happens to be on java's whitelist.
-wmname = "LG3D"
-
-start_up_comands = [
-    "picom --experimental-backends -b",
-    "setxkbmap latam",
-    "feh --bg-fill /home/yulian/.config/qtile/6zsqbsxu5nka1.jpg",
-]
-
-for i in start_up_comands:
-    os.system(i)
+            ]
+    return widgets
